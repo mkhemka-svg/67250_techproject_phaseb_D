1621d440cf654ca61a028aeb263e8346ebd659d6
mkhemka-svg <mkhemka@andrew.cmu.edu>
2026-03-17 16:22:04 -0400

    nd

--- mkhemka-increment3/static/script.js
+++ mkhemka-increment3/static/script.js
@@ -28,7 +28,7 @@ if (C.length > z) {
 }
 
 var L1 = ["Watermelon","Pineapple","Pear","Banana"];
-var L2 = ["Apple","Banana","Kiwi","Orange"];
+var L2 = ["Apple","Kiwi","Orange"];
 
 function findTheBanana(array) {
     var i
